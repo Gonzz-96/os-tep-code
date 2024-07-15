d2f8b4d4ff459239ec035894bb0ef9519ea96099
Gonzalo Campos <gonzalocampose1@gmail.com>
2024-07-15 19:34:41 +0000

    Wait system call.

---
+++ virtualization/chapter5/p2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]) {
+    printf("hello (parent pid:%d)\n", (int) getpid());
+    int rc = fork();
+
+    if (rc < 0) {
+        // fork failed
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        // child process (new process)
+        printf("child (pid:%d)\n", (int) getpid());
+    } else {
+        int rc_wait = wait(NULL);
+        printf("parent of %d (rc_wait:%d) (parent pid:%d)\n", rc, rc_wait, (int) getpid());
+    }
+    return 0;
+}
+
